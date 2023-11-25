9ed3282fa4f9ba781ca635d0a15e7fed72e49039
maryam-hussien <maryamhussien122@gmail.com>
2023-11-25 20:11:32 +0200

    backend && file structure

---
+++ BackEnd/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "User",
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
-
+import {ConfigProvider} from "antd"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter> 
+    <ConfigProvider
+    theme={{
+      components:{
+        Button:{
+          colorPrimary:"#111111",
+          colorPrimaryHover:"#aaaaaa"
+        }
+      }
+    }}
+    >
+    <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
