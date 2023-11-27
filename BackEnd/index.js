6872d03a97201e0a363ee12544f0263252b6f0f0
maryam-hussien <maryamhussien122@gmail.com>
2023-11-27 23:25:55 +0200

    item api

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const router = require("./routes/userRoutrs")
+const userRoutes = require("./routes/userRoutrs")
+const itemRoutes = require("./routes/itemRoutes")
 const bodyParser = require('body-parser');
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
@@ -8,6 +9,7 @@ const port = process.env.PORT || 8000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/user' , router)
+app.use('/user' , userRoutes)
+app.use('/item' , itemRoutes)
 
 app.listen(port,() => console.log(`nodejs server started on port ${port}`))
--- BackEnd/routes/userRoutrs.js
+++ BackEnd/routes/userRoutrs.js
@@ -1,15 +1,16 @@
-const router = require("express").Router();
+const userRoutes = require("express").Router();
 const  { registerUser,LoginUser,GetMe , editUser , deleteUser , gettAll, getAll} = require("../controllers/UserController")
 const {protect} = require('../middleware/AuthMiddleware')
 
-router.post("/register" , registerUser);
+userRoutes.post("/register" , registerUser);
 
-router.post("/login" , LoginUser);
+userRoutes.post("/login" , LoginUser);
 
 //generate jwt 
-router.get('/me' , protect ,GetMe)
+userRoutes.get('/me' , protect ,GetMe)
 
-router.put('/edit' , protect , editUser)
-router.delete('/delete' , protect , deleteUser)
-router.get('/all', protect ,getAll)
-module.exports = router;
+userRoutes.put('/edit' , protect , editUser)
+userRoutes.delete('/delete' , protect , deleteUser)
+userRoutes.get('/all', protect ,getAll)
+
+module.exports = userRoutes;
