80484ef8193ca5b3465812a57eac7b63fa68f83a
menna544 <116039213+menna544@users.noreply.github.com>
2023-11-30 00:33:46 +0200

    responsive home

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,18 +1,18 @@
 import {Routes, Route } from "react-router-dom";
 import "./App.css";
- //import Home from "./dashboard/pages/home/Home";
- import Home1 from "./pages/Home&Ditails/Home";
- //import Authentication from "./pages/Signinup/Authentication";
-//import Checkout from "./pages/checkout/Checkout";
+ import Home from "./dashboard/pages/home/Home";
+ import Home1 from "./pages/Home/Home";
+ import Authentication from "./pages/Signinup/Authentication";
+import Checkout from "./pages/checkout/Checkout";
 function App() {
   return (
     <div className="App">
       
       <Routes >
       <Route path="/" element={<Home1 />}/>
-        {/* <Route path="/" element={<Checkout />}/>*/}
-       {/* <Route path="/" element={<Authentication />}/>*/}
-        {/*<Route path="/dashboard/home" element={<Home1 />}/>*/}
+         <Route path="/cart/checkout" element={<Checkout />}/>
+        <Route path="/auth" element={<Authentication />}/>
+        <Route path="/dashboard/home" element={<Home />}/>
       </Routes>
     </div>
   );
