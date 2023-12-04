9651a3436cbf796711a9625b249fc210eb8f3f16
maryam-hussien <maryamhussien122@gmail.com>
2023-12-04 19:18:20 +0200

    backend edits

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,18 +1,16 @@
 import {Routes, Route } from "react-router-dom";
-import "./App.css";
- //import Home from "./dashboard/pages/home/Home";
  import Home1 from "./pages/Home&Ditails/Home";
- //import Authentication from "./pages/Signinup/Authentication";
-//import Checkout from "./pages/checkout/Checkout";
+ import Authentication from "./pages/Signinup/Authentication";
+import Checkout from "./pages/checkout/Checkout";
 function App() {
   return (
     <div className="App">
       
       <Routes >
       <Route path="/" element={<Home1 />}/>
-        {/* <Route path="/" element={<Checkout />}/>*/}
-       {/* <Route path="/" element={<Authentication />}/>*/}
-        {/*<Route path="/dashboard/home" element={<Home1 />}/>*/}
+        <Route path="/home/cart/checkout" element={<Checkout />}/>
+        <Route path="/auth" element={<Authentication />}/>
+        <Route path="/dashboard/home" element={<Home1 />}/>
       </Routes>
     </div>
   );
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import {ConfigProvider} from "antd"
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,5 +23,4 @@ root.render(
   </React.StrictMode>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
