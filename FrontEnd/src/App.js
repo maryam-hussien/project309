6ccac6bbfee6a55c3b4d0640c6e46f692ba42078
maryam-hussien <maryamhussien122@gmail.com>
2023-12-05 17:42:34 +0200

    edits

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,21 +1,13 @@
 import {Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
- import Home from "./dashboard/pages/home/DashHome";
-=======
->>>>>>> bd0520c4b0b18482953917250c9de28da725591d
- import Home1 from "./pages/Home/Home";
+ import Home from "./pages/Home/Home";
  import Authentication from "./pages/Signinup/Authentication";
 import Checkout from "./pages/checkout/Checkout";
 import "./app.css"
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      
-=======
->>>>>>> bd0520c4b0b18482953917250c9de28da725591d
        <Routes >
-      <Route path="/" element={<Home1 />}/>
+      <Route path="/" element={<Home />}/>
          <Route path="/cart/checkout" element={<Checkout />}/>
         <Route path="/auth" element={<Authentication />}/>
       </Routes>
