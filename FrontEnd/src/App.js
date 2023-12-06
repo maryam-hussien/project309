a31cadc2a096aaa4e8018a1f56f6d56e6eb3cd13
maryam-hussien <maryamhussien122@gmail.com>
2023-12-06 20:56:12 +0200

    fix routes

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -9,14 +9,19 @@ import User from "./dashboard/pages/User/User";
 import Product from "./dashboard/pages/Product/Product";
 import NewProduct from "./dashboard/pages/NewProducts/NewProduct";
 import DProductList from "./dashboard/pages/DProductList/DProductList";
+import Navbar from "./componenet/Navbar/Navbar"
+import Footer from "./componenet/footer/Footer"
+import Details from "./pages/Details/Details"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart/checkout" element={<Checkout />} />
         <Route path="/auth" element={<Authentication />} />
+        <Route path="/details" element={<Details />}/>
         <Route path="/dashboard/home" element={<HomePage />} />
         <Route path="/dashboard/users" element={<UserList />} />
         <Route path="/dashboard/orderList" element={<OrderList />} />
@@ -26,6 +31,7 @@ function App() {
         <Route path="/dashboard/product/:productId" element={<Product />} />
         <Route path="/dashboard/newproduct" element={<NewProduct />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
