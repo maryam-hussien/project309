ff45891ad02e5ea476cf9c55525b81c775857520
mariemmohamed374 <marimmemo849@gmail.com>
2023-12-07 21:19:34 +0200

    add cart page

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -9,6 +9,7 @@ import User from "./dashboard/pages/User/User";
 import Product from "./dashboard/pages/Product/Product";
 import NewProduct from "./dashboard/pages/NewProducts/NewProduct";
 import DProductList from "./dashboard/pages/DProductList/DProductList";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
@@ -25,6 +26,8 @@ function App() {
         <Route path="/dashboard/product" element={<Product />} />
         <Route path="/dashboard/product/:productId" element={<Product />} />
         <Route path="/dashboard/newproduct" element={<NewProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        
       </Routes>
     </div>
   );
---
+++ FrontEnd/src/pages/Cart/Cart.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import "./Cart.css";
+import placeholderImage from "../../assets/profile/profile.jpg";
+import Navbar from "../../componenet/Navbar/Navbar";
+import Footer from "../../componenet/footer/Footer";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { Link } from "react-router-dom";
+
+
+function Cart() {
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: "Product 1", price: 20.0, quantity: 2 },
+    { id: 2, name: "Product 2", price: 15.0, quantity: 1 },
+    { id: 3, name: "Product 3", price: 25.0, quantity: 3 },
+  ]);
+
+  const removeItem = (id) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.filter((item) => item.id !== id)
+    );
+  };
+
+  const updateQuantity = (id, newQuantity) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.map((item) =>
+        item.id === id ? { ...item, quantity: newQuantity } : item
+      )
+    );
+  };
+
+  const getTotalPrice = () => {
+    return cartItems
+      .reduce((total, item) => total + item.price * item.quantity, 0)
+      .toFixed(2);
+  };
+
+  // const handleCheckout = () => {
+
+  //   alert("Checkout functionality will be implemented here!");
+  // };
+
+  return (
+    <>
+      <Navbar />
+      <div className="cart-container">
+        <h2>Your Cart</h2>
+
+        {cartItems.map((item) => (
+          <div key={item.id} className="cart-item">
+            <div className="product-info">
+              <img src={placeholderImage} alt={`Product ${item.id}`} />
+              <div className="item-details">
+                <span className="product-name">{item.name}</span>
+                <span className="product-price">${item.price.toFixed(2)}</span>
+              </div>
+            </div>
+            <div className="quantity">
+              <span>Quantity:</span>
+              <input
+                type="number"
+                value={item.quantity}
+                min="1"
+                onChange={(e) =>
+                  updateQuantity(item.id, parseInt(e.target.value, 10))
+                }
+              />
+            </div>
+            <div className ="delete-button">
+            <DeleteOutlineIcon
+              onClick={() => removeItem(item.id)}
+            />
+            </div>
+            
+          </div>
+        ))}
+
+        <div className="total-price">
+          <span>Total:</span>
+          <span>${getTotalPrice()}</span>
+        </div>
+        <Link to ="/cart/checkout">
+        <button className="checkout-button" >
+          Checkout
+        </button>
+        </Link>
+        {/* <div className="fn">
+
+      </div> */}
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Cart;
